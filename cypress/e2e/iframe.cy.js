29e3c225eac65fe8519bfb4da85f9833cb6b2540
Stephanie Peñaloza <spenalozacortes@gmail.com>
2024-04-30 15:49:36 -0600

    Complete iframe test case

--- cypress/e2e/iframe.cy.js
+++ cypress/e2e/iframe.cy.js
@@ -1,2 +1,15 @@
 /// <reference types = "Cypress" />
 
+import 'cypress-iframe';
+
+describe('Iframe tests', function() {
+    it('test 1', function() {
+        cy.visit('https://demoqa.com/');
+        cy.contains('Alerts, Frame & Windows').parent().click();
+        cy.contains('Frames').click();
+        cy.get('#framesWrapper').should('exist');
+        cy.iframe('#frame1').find('#sampleHeading').invoke('text').then(firstText => {
+            cy.iframe('#frame2').find('#sampleHeading').invoke('text').should('eq', firstText);
+        });
+    });
+});
--- cypress/e2e/pdf.cy.js
+++ cypress/e2e/pdf.cy.js
@@ -1,7 +1,7 @@
 /// <reference types = "Cypress" />
 
 describe('PDF tests', function() {
-    it.only('test 1', function() {
+    it('test 1', function() {
         cy.visit('https://www.sharedfilespro.com/download-sample-pdf/');
         cy.get('a.shared-files-download-button').click();
         cy.readFile('./cypress/downloads/sample.pdf'); // waiting for the file to be downloaded
@@ -15,7 +15,7 @@ describe('PDF tests', function() {
     it('test2', function () {
         cy.visit('https://freetestdata.com/document-files/pdf/');
         cy.contains('.elementor-inner-section', '100 KB').find('.elementor-button').click();
-        cy.readFile('./cypress/downloads/Free_Test_Data_100KB_PDF.pdf'); // waiting for the file to be downloaded
+        cy.readFile('./cypress/downloads/Free_Test_Data_100KB_PDF.pdf'); 
         cy.task('readPdf', './cypress/downloads/Free_Test_Data_100KB_PDF.pdf').then(data => {
             expect(data.text).to.not.be.empty;
         });
